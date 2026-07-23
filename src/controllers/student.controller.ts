d81596364f6a0fbc4b6a5c0f18e398a726a0a472
Santosh Bhandari <santooshbhandari88@gmail.com>
2026-07-23 23:58:59 +0545

    Crud Operation from student

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -4,8 +4,10 @@ import Student from "../models/student.model";
 import { sendResponse } from "../utils/sendResponse.utils";
 import { ApiError } from "../utils/ApiError.utils";
 import User from "../models/user.model";
+import bcrypt from "bcryptjs";
+import { hash } from "../utils/bcrypt.utils";
 
-export const getStudnet = catchAsync(
+export const getStudent = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { userId } = req.body;
 
@@ -25,20 +27,86 @@ export const getStudnet = catchAsync(
 
 export const updateStudent = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-        const {name, email ,password, parentName, parentPhone,address,userId } =req.body;
+    const { name, email, password, parentName, parentPhone, address, userId } =
+      req.body;
 
-        const user = await User.findOne({user:userId});
-        const student = await Student.findOne({user:userId});
-        
+    const user = await User.findOne({ user: userId });
+    const student = await Student.findOne({ user: userId });
 
-        if(!student){
-                throw new ApiError("Student is not found",404);
-        }
+    if (!student) {
+      throw new ApiError("Student is not found", 404);
+    }
+    if (!user) {
+      throw new ApiError("User is not found", 404);
+    }
 
-        // if(name) user.name= name;
-        // if(parentName) student.parentName=parentName;
+    if (name) user.name = name;
+    if (email) user.email = email;
+    if (parentName) student.parentName = parentName;
+    if (parentPhone) student.parentPhone = parentPhone;
+    if (address) student.address = address;
 
+    await user.save();
 
-  
-}
+    await student.save();
+
+    sendResponse(res, {
+      message: "Update record Successfully",
+      data: {
+        student,
+        user,
+      },
+      statusCode: 201,
+    });
+  },
+);
+
+export const changePassword = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { oldpassword, newpassword, userId } = req.body;
+
+    const user = await User.findOne({ user: userId }).select("+password");
+    if (!user) {
+      throw new ApiError("User is not found", 404);
+    }
+
+    const ismatch = bcrypt.compare(oldpassword, user.password);
+
+    if (!ismatch) {
+      throw new ApiError("Password is incorrect", 404);
+    }
+
+    const newhashpass = await hash(newpassword);
+    user.password = newhashpass;
+
+    await user.save();
+
+    sendResponse(res, {
+      message: "Password Change successfully",
+      data: user,
+      statusCode: 201,
+    });
+  },
+);
+
+export const getProfile = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { userId } = req.body;
+
+    const user = await User.findOne({ user: userId }).select("-password");
+    const student = await Student.findOne({ user: userId });
+
+    if (!student || !user) {
+      throw new ApiError("Student not found", 404);
+    }
+
+    sendResponse(res, {
+      message: "Your Record fetched",
+      data: {
+        student,
+        User,
+      },
+      statusCode: 201,
+    });
+  },
 );
