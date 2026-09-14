8b392117f5130e31a92ea814b4f5133eb12ba296
Santosh Bhandari <santooshbhandari88@gmail.com>
2026-09-14 23:28:31 +0545

    class modules with validator

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -6,17 +6,25 @@ import {
   getClassById,
   updateClass,
 } from "../controllers/class.controller";
+import { validate } from "../Middlewares/validator.middleware";
+import {
+  classSchema,
+  createClassSchema,
+  deleteClassSchema,
+  getByIdClassSchema,
+  updateClassSchema,
+} from "../validators/class.validatore";
 
 const router = express.Router();
 
-router.get("/", getAllClass);
+router.get("/", validate(classSchema), getAllClass);
 
-router.get("/:id", getClassById);
+router.get("/:id", validate(getByIdClassSchema), getClassById);
 
-router.post("/", createClass);
+router.post("/", validate(createClassSchema), createClass);
 
-router.put("/:id", updateClass);
+router.put("/:id", validate(updateClassSchema), updateClass);
 
-router.delete("/:id", deleteClass);
+router.delete("/:id", validate(deleteClassSchema), deleteClass);
 
 export default router;
--- src/validators/class.validatore.ts
+++ src/validators/class.validatore.ts
@@ -1,28 +1,8 @@
+import mongoose from "mongoose";
 import z, { input } from "zod";
 
-export const createStudentSchema = z.object({
+export const createClassSchema = z.object({
   body: z.object({
-    name: z
-      .string({
-        error: (issue) => {
-          issue.input === null
-            ? "full name is required"
-            : "full name must be string";
-        },
-      })
-      .min(3, "name must be 2 character long")
-      .max(100, "name should not exceeds 100 characters long")
-      .trim(),
-
-    email: z.email({
-      error: (issue) =>
-        issue.input === undefined ? "email is reauired" : "Invalid credintals",
-    }),
-    password: z.string({
-      error: (issue) =>
-        issue.input === null ? "password is required" : "Invalid credintals",
-    }),
-
     classname: z
       .string({
         error: (issue) => {
@@ -34,5 +14,82 @@ export const createStudentSchema = z.object({
       .min(3, "name must be 2 character long")
       .max(100, "name should not exceeds 100 characters long")
       .trim(),
+
+    section: z.string({
+      error: (issue) =>
+        issue.input === undefined
+          ? "section is reauired"
+          : "section must be string",
+    }),
+    room_no: z.coerce
+      .number({
+        error: (issue) =>
+          issue.input === null ? "password is required" : "Invalid credintals",
+      })
+      .positive("number should be positive"),
+
+    teacher: z
+      .string({
+        error: "teacher must be string",
+      })
+      .refine(
+        (id) => mongoose.Types.ObjectId.isValid(id),
+        "teacher is isnot found",
+      ),
+  }),
+});
+
+export const updateClassSchema = z.object({
+  body: z.object({
+    classname: z
+      .string({
+        error: "class name must be string",
+      })
+      .optional(),
+
+    room_no: z.coerce
+      .number({
+        error: "room_no must be number",
+      })
+      .positive()
+      .optional(),
+
+    section: z
+      .string({
+        error: "section must be string",
+      })
+      .optional(),
+
+    teacher: z
+      .string({
+        error: "teacher is must be string",
+      })
+      .refine((id) => mongoose.Types.ObjectId.isValid(id), "invalid id"),
+  }),
+});
+
+export const deleteClassSchema = z.object({
+  params: z.object({
+    id: z
+      .string()
+      .refine((id) => mongoose.Types.ObjectId.isValid(id), "invalid id"),
+  }),
+});
+
+export const getByIdClassSchema = z.object({
+  params: z.object({
+    id: z
+      .string()
+      .refine((id) => mongoose.Types.ObjectId.isValid(id), "invalid id"),
+  }),
+});
+
+export const classSchema = z.object({
+  query: z.object({
+    query: z.string().optional(),
+    order: z.enum(["DESC", "ASC"]).default("DESC"),
+    sortBy: z.string().default("createdAt"),
+    limit: z.coerce.number().int().min(1).max(100).default(10),
+    page:z.coerce.number().int().min(1).default(1),
   }),
 });
